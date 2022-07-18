17e8fbf5f174f19786bec0f81a5651fbedef49c4
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-07-18 22:25:57 +0300

    Revert "add contacts"
    This reverts commit 0f94247201e78f5825b355281cb89a723d1a9304.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,16 @@
-import {Container } from 'react-bootstrap';
-import ContactForm from './ContactsForm/ContactForm';
-import ContactList from './ContactList/ContactList';
-import Filter from './Filter/Filter';
-
 export const App = () => {
   return (
-    <>
-    <Container className="p-3">
-    <h1 className="header">Phonebook</h1>
-      <ContactForm />
-      </Container>
-      <Container className="p-3">
-      <h2 className="header">Contacts</h2>
-      <Filter />
-      <ContactList />
-      </Container>
-    </>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
